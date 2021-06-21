9089526338d94dc5da4f8ba90c2217f9a1945a54
Sebin ms <sebinkelakam@gmail.com>
2021-06-21 21:54:36 +0500

    Add files via upload

---
+++ even of numbers ( for while).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+for(n=1;n<=100;n++)
+{
+   if(number%2==0)
+}
+    {
+    printf("the number %d",n);
+
+    }
+
+}
---
+++ sum of n numbers(for while).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int i,n,s=0;
+    printf("enter the number\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        s=s+i;
+    }
+    {
+       printf("sum is \n  %d",s);
+    }
+
+}
+
+
+
